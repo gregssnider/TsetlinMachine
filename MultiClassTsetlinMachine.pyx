b465d3b590a09d7246a5c6f17ea2a1a9f0b18915
greg <greg_s_snider@outlook.com>
2018-04-11 14:33:08 -0700

    Cython code cleanup.

--- MultiClassTsetlinMachine.pyx
+++ MultiClassTsetlinMachine.pyx
@@ -334,14 +334,14 @@ cdef class MultiClassTsetlinMachine:
                 elif clause_output == 1:
                     self.get_random_values()
                     for k in xrange(self.number_of_features):
-                        if self.random_values[k] <= 1.0 * (self.s - 1) / self.s:
-                            if X[k] == 1:
+                        if X[k] == 1:
+                            if self.random_values[k] <= 1.0 * (self.s - 1) / self.s:
                                 self.ta_state[j, k] += 1
 
                     self.get_random_values()
                     for k in xrange(self.number_of_features):
-                        if self.random_values[k] <= 1.0/self.s:
-                            if X[k] == 0:
+                        if X[k] == 0:
+                            if self.random_values[k] <= 1.0/self.s:
                                 self.ta_state[j, k] -= 1
 
             elif feedback < 0:
