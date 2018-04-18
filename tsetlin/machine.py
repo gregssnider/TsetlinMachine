0eaa0bfc08ef11e26bf1d197b69c7cda3c14ef21
greg <greg_s_snider@outlook.com>
2018-04-18 15:09:28 -0700

    Tuning train

--- tsetlin/machine.py
+++ tsetlin/machine.py
@@ -310,8 +310,11 @@ class TsetlinMachine2:
         pos_delta_inv = clause_matrix * (inv_X * high_prob)
         neg_delta_inv = clause_matrix * (X * low_prob)
 
-        self.automata += pos_feedback_matrix * (pos_delta - neg_delta - neg_low_delta) + \
-                         neg_feedback_matrix * (clause_matrix * inv_X * ((self.action ^ 1).int()))
+        self.automata += pos_feedback_matrix * pos_delta
+        self.automata -= pos_feedback_matrix * (neg_delta + neg_low_delta)
+        self.automata += neg_feedback_matrix * (clause_matrix * inv_X * ((self.action ^ 1).int()))
+        #self.automata += pos_feedback_matrix * (pos_delta - neg_delta - neg_low_delta) + \
+        #                 neg_feedback_matrix * (clause_matrix * inv_X * ((self.action ^ 1).int()))
 
         self.inv_automata += pos_feedback_matrix * (pos_delta_inv - neg_delta_inv - neg_low_delta) + \
                              neg_feedback_matrix * clause_matrix * X * ((self.inv_action ^ 1).int())
